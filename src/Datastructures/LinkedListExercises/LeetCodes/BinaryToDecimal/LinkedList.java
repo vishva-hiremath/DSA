7684b4e3f97d2c6d9c43bd2bebf1b69334252607
vishva-hiremath <vishvamh@gmail.com>
2025-03-26 18:41:11 -0500

    Binary to decimal

---
+++ src/Datastructures/LinkedListExercises/LeetCodes/BinaryToDecimal/LinkedList.java
@@ -0,0 +1,83 @@
+package Datastructures.LinkedListExercises.LeetCodes.BinaryToDecimal;
+
+public class LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    public int binaryToDecimal(){
+        Node current = head;
+        //int  temp = length == 1 ? 1:length*2; // U can use this method as well.
+        int decimal = 0;
+        while (current != null) {
+           // decimal += (current.value*temp);
+            decimal = decimal *2 + current.value;
+            current = current.next;
+            //temp /= 2;
+        }
+        return decimal;
+    }
+
+
+}
+
+
+
